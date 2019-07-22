e98c173ec4ff7e3a4e8192069c590a68c777878c
wangzhen <1907144924@qq.com>
2019-07-22 18:40:25 +0800

    搞定

--- app/src/main/java/com/example/admin/testdemo2/DragScaleViewInner.java
+++ app/src/main/java/com/example/admin/testdemo2/DragScaleViewInner.java
@@ -7,6 +7,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.DashPathEffect;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.util.AttributeSet;
@@ -14,8 +15,8 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 /**
  * 本来准备直接添加ImageView ，但是选中的边框线绘制的时候会显示在最上层，盖住了小图标，然后准备在dispatchDraw()方法里面重新的去动态添加ImageView
@@ -27,6 +28,7 @@ public class DragScaleViewInner extends RelativeLayout {
     public final static int OPTION_DRAG = 1;//拖动
     public final static int OPTION_DELETE = 2;//删除
     public final static int OPTION_SCALE = 3;//缩放
+    public final static int MIN_INTERVAL = 500 ; //最小间隔时间
 
     private Bitmap bitmap_drag;
     private Bitmap bitmap_delete;
@@ -38,7 +40,10 @@ public class DragScaleViewInner extends RelativeLayout {
     private long firstClick;//记录第一次单击事件
     private ScaleGestureDetector mScaleGestureDetector = null;
     private int option;
-    private MotionEvent event_down;//按下位置
+    private float oldDistance, newDistance; //缩放比例
+    private float lastX, lastY;
+    private float offset, icon_bitmap_width;
+    private int aspectRatio ; //子view的宽高比
 
     public void setSelectStatus(boolean selectStatus) {
         //如果当前view的默认的选中状态和选中的状态是不一样的，此时我们修改选中状态
@@ -72,7 +77,6 @@ public class DragScaleViewInner extends RelativeLayout {
     }
 
     private void initData() {
-        setClipChildren(true);
         paint_rect = new Paint();
         paint_rect.setStrokeWidth(1);
         paint_rect.setStyle(Paint.Style.STROKE);
@@ -82,15 +86,31 @@ public class DragScaleViewInner extends RelativeLayout {
         paint_icon = new Paint();
         paint_icon.setAntiAlias(true);
         Resources resources = getResources();
-        bitmap_delete = BitmapFactory.decodeResource(resources, R.mipmap.sticker_delete);
-        bitmap_drag = BitmapFactory.decodeResource(resources, R.mipmap.sticker_drag);
+        bitmap_delete = BitmapFactory.decodeResource(resources, R.mipmap.icon_delete);
+        bitmap_drag = BitmapFactory.decodeResource(resources, R.mipmap.icon_scale);
+        offset = bitmap_delete.getWidth() / 2;
+        icon_bitmap_width = bitmap_delete.getWidth();
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        //必须要测量子布局的宽高
-        measureChildren(widthMeasureSpec, heightMeasureSpec);
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        //必须要测量子布局的宽高
+        int count = getChildCount();
+        for (int i = 0; i < count; i++) {
+            View child = getChildAt(i);
+            //经过这个方法(或者 measureChild) ， child 的宽高都是有值的
+            measureChildWithMargins(child, widthMeasureSpec, 0, heightMeasureSpec, 0);
+        }
+        int maxHeight = 0;
+        int maxWidth = 0;
+        for (int i = 0; i < getChildCount(); i++) {
+            View view = getChildAt(i);
+            maxHeight = view.getMeasuredHeight() > maxHeight ? view.getMeasuredHeight() : maxHeight;
+            maxWidth = view.getMeasuredWidth() > maxWidth ? view.getMeasuredWidth() : maxWidth;
+        }
+        aspectRatio = maxWidth / maxHeight;//计算宽高比
+        setMeasuredDimension(maxWidth + bitmap_delete.getWidth(), maxHeight + bitmap_delete.getHeight());
     }
 
     @Override
@@ -102,35 +122,43 @@ public class DragScaleViewInner extends RelativeLayout {
             String tag = view.getTag() + "";
             switch (tag) {
                 case "main":
-                    view.layout(20, 20, view.getWidth() + 20, view.getHeight() + 20);
+                    view.layout((int) offset, (int) offset, (int) (view.getWidth() + offset), (int) (view.getHeight() + offset));
                     break;
                 default:
                     break;
             }
         }
-
     }
 
     @Override
     protected void dispatchDraw(Canvas canvas) {
         super.dispatchDraw(canvas);
         if (selectStatus) {
-            canvas.drawRect(19, 19, getWidth() - 19, getHeight() - 19, paint_rect);//选中线框
+            canvas.drawRect(offset - 1, offset - 1, getWidth() - offset + 1, getHeight() - offset + 1, paint_rect);//选中线框
             canvas.drawBitmap(bitmap_delete, 0, 0, paint_icon);
-            canvas.drawBitmap(bitmap_drag, getWidth() - 40, getHeight() - 40, paint_icon);
+            canvas.drawBitmap(bitmap_drag, getWidth() - icon_bitmap_width, getHeight() - icon_bitmap_width, paint_icon);
         }
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        rectF_delete = new RectF(0, 0, 40, 40);
-        rectF_scale = new RectF(getWidth() - 40, getHeight() - 40, getHeight(), getHeight());
-        rectF_drag = new RectF(20, 20, getWidth() - 20, getHeight() - 20);
+        rectF_delete = new RectF(0, 0, icon_bitmap_width, icon_bitmap_width);
+        rectF_scale = new RectF(getWidth() - icon_bitmap_width, getHeight() - icon_bitmap_width, getWidth(), getHeight());
+        rectF_drag = new RectF(offset, offset, getWidth() - offset, getHeight() - offset);
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN://按下
                 if (selectStatus) {
-                    event_down = event;
+                    lastX = event.getRawX();
+                    lastY = event.getRawY();
+                    oldDistance = caculateDistance(lastX, lastY);
                     handleDown(event);
+                    if(option == OPTION_DRAG){
+                        long interval = System.currentTimeMillis() - firstClick;
+                        if(interval < MIN_INTERVAL){//如果两次点击间隔小于最小间隔时间，那就判定双击
+
+                        }
+                        firstClick = System.currentTimeMillis();//重新记录点击时间
+                    }
                 }
                 break;
             case MotionEvent.ACTION_UP://抬起
@@ -138,13 +166,18 @@ public class DragScaleViewInner extends RelativeLayout {
                     onDragScaleCallBackListener.dragScaleCallBack(getTag() + "");
                 }
                 break;
-            case MotionEvent.ACTION_MOVE://拖动
+            case MotionEvent.ACTION_MOVE://移动
                 if (selectStatus) {
                     if (option == OPTION_DRAG) {
                         //拖动
+                        moveView(event.getRawX(), event.getRawY());
                     } else if (option == OPTION_SCALE) {
-                       scale(event);
+                        newDistance = caculateDistance(event.getRawX(), event.getRawY());
+                        zoomImage();
                     }
+                    lastX = event.getRawX();//重新刷新上次移动点X的位置
+                    lastY = event.getRawY();//重新刷新上次移动点Y的位置
+                    oldDistance = caculateDistance(lastX, lastY);
                 }
                 break;
             default:
@@ -153,21 +186,52 @@ public class DragScaleViewInner extends RelativeLayout {
         return true;//消费事件
     }
 
-    private void scale(MotionEvent event) {
-        Log.i("MDL","scale");
-        float xDistance = event.getX() - event_down.getX();
-        float yDistance = event.getY() -  event_down.getY();
-        //float bevel = (float) Math.sqrt(Math.pow(xDistance , 2) + Math.pow(yDistance , 2));
-        RelativeLayout.LayoutParams layoutParams = (LayoutParams) getLayoutParams();
-        layoutParams.width = getWidth() * 2;
-        layoutParams.height = getHeight() * 2;
+    /**
+     * 移动 ，移动的是此view的下位置
+     *
+     * @param rawX
+     * @param rawY
+     */
+    private void moveView(float rawX, float rawY) {
+        //重新设置位置
+        layout(getLeft() + (int) (rawX - lastX), getTop() + (int) (rawY - lastY), getRight() + (int) (rawX - lastX), getBottom() + (int) (rawY - lastY));
+        //设置此view在父布局的位置
+        LayoutParams layoutParams = (LayoutParams) getLayoutParams();
+        layoutParams.leftMargin = getLeft();
+        layoutParams.topMargin = getTop();
         setLayoutParams(layoutParams);
-        View view = getChildAt(0);
-        LayoutParams layoutParams1 = (LayoutParams) view.getLayoutParams();
-        layoutParams1.width = layoutParams1.width * 2;
-        layoutParams1.height = layoutParams1.height * 22;
-        view.setLayoutParams(layoutParams1);
-        postInvalidate();
+    }
+
+    /**
+     * 缩放
+     * 我这个viewgroup设置的是wrap_content包裹内容，所以缩放的时候只需要缩放imageView的宽高即可
+     */
+    private void zoomImage() {
+        float scaleRatio = newDistance / oldDistance;
+        float newWidthParent = getWidth() * scaleRatio;
+        float newHeightParent = newWidthParent / aspectRatio;
+        if (newWidthParent > bitmap_drag.getWidth() * 2 || newHeightParent > bitmap_drag.getHeight() * 2) {
+            //重新设置此view的布局位置
+            LayoutParams layoutParamsParent = (LayoutParams) getLayoutParams();
+            layoutParamsParent.width = (int) newWidthParent;
+            layoutParamsParent.height = (int) newHeightParent;
+            layoutParamsParent.leftMargin = getLeft();
+            layoutParamsParent.topMargin = getTop();
+            setLayoutParams(layoutParamsParent);
+
+            //重新设置子view的宽高
+            View view = getChildAt(0);
+            float newWidth = view.getWidth() * scaleRatio;
+            float newHeight = newWidth / aspectRatio;
+            LayoutParams layoutParams = (LayoutParams) view.getLayoutParams();
+            layoutParams.width = (int) newWidth;
+            layoutParams.height = (int) newHeight;
+            view.setLayoutParams(layoutParams);
+        }
+    }
+
+    private float caculateDistance(float x, float y) {
+        return (float) Math.sqrt(x * x + y * y);
     }
 
     /**
--- app/src/main/java/com/example/admin/testdemo2/MainActivity.java
+++ app/src/main/java/com/example/admin/testdemo2/MainActivity.java
@@ -5,11 +5,13 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements OnDragScaleCallBackListener {
 
@@ -26,30 +28,29 @@ public class MainActivity extends AppCompatActivity implements OnDragScaleCallBa
         ImageView imageView = new ImageView(this);
         imageView.setImageResource(R.mipmap.ic_launcher);
         imageView.setTag("main");
-        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(300, 300);
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(300, 110);
         layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT);
         imageView.setLayoutParams(layoutParams);
         DragScaleViewInner dragScaleViewInner = new DragScaleViewInner(this);
-        RelativeLayout.LayoutParams layoutParamsInner = new RelativeLayout.LayoutParams(300 + 40, 300 + 40);
-        layoutParamsInner.addRule(RelativeLayout.CENTER_IN_PARENT);
+        RelativeLayout.LayoutParams layoutParamsInner = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         dragScaleViewInner.setLayoutParams(layoutParamsInner);
         dragScaleViewInner.addView(imageView);
         dragScaleViewInner.setTag("dragScaleViewInner");
         dragScaleViewInner.setOnDragScaleCallBackListener(this);
         relativeLayout.addView(dragScaleViewInner);
 
-        ImageView imageView2 = new ImageView(this);
-        imageView2.setImageResource(R.mipmap.ic_launcher);
-        imageView2.setTag("main");
-        RelativeLayout.LayoutParams layoutParams2 = new RelativeLayout.LayoutParams(300, 300);
-        layoutParams2.addRule(RelativeLayout.CENTER_IN_PARENT);
-        imageView2.setLayoutParams(layoutParams2);
+        TextView textView = new TextView(this);
+        textView.setText("世界如此美好");
+        textView.setTag("main");
+        RelativeLayout.LayoutParams layoutParams2 = new RelativeLayout.LayoutParams(400, 400);
+        textView.setGravity(Gravity.CENTER);
+        textView.setLayoutParams(layoutParams2);
+        textView.setBackgroundColor(Color.RED);
         DragScaleViewInner dragScaleViewInner2 = new DragScaleViewInner(this);
-        RelativeLayout.LayoutParams layoutParamsInner2 = new RelativeLayout.LayoutParams(300 + 40, 300 + 40);
-        layoutParamsInner2.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
-        layoutParamsInner2.setMargins(20,0,0,0);
+        RelativeLayout.LayoutParams layoutParamsInner2 = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParamsInner2.setMargins(20, 0, 0, 0);
         dragScaleViewInner2.setLayoutParams(layoutParamsInner2);
-        dragScaleViewInner2.addView(imageView2);
+        dragScaleViewInner2.addView(textView);
         dragScaleViewInner2.setTag("dragScaleViewInner2");
         dragScaleViewInner2.setOnDragScaleCallBackListener(this);
         relativeLayout.addView(dragScaleViewInner2);
